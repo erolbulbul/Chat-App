6bf367717bb0e6a8244696e890bb6b7942df39f5
erolbulbul <97367511+erolbulbul@users.noreply.github.com>
2023-01-12 02:56:53 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import { ChatEngine } from "react-chat-engine";
+import './App.css';
+
+
+const App =() => {
+    return(
+     <ChatEngine
+      height="100vh"
+      projectID="
+      a7148985-bc99-4462-ad9c-58c900c2fec9"
+      userName="erol  "
+      userSecret="123123 "
+      
+     />
+    );
+}
+export default App;
